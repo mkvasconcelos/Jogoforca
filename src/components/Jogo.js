f627fbcb19a7be6dc130c967f049ab5394bda041
Mateus Vasconcelos <mateuskvasconcelos@gmail.com>
2022-11-22 19:02:30 -0300

    CSS of the start button working

---
+++ src/components/Jogo.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Jogo() {
+  return (
+    <main className="jogo">
+      <section>
+        <img src="assets/forca0.png" alt="imagem-forca"></img>
+      </section>
+      <section>
+        <button>Escolher Palavra</button>
+        <span>_ _ _ _ _ _ _ _</span>
+      </section>
+    </main>
+  );
+}
